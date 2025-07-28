5e9c6c2928371cfbea48ffa3533220eb0cbe43c7
influence20 <donblinksdan@gmail.com>
2025-07-28 17:33:15 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~wisAGZ7Y#d1BtTa7cTpOtfTMuamBpPACCqfmggKsExS74Zwy2ung",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "2349136746608", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
